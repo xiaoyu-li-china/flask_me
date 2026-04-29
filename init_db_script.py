bfaba68191367ab55f93b0423520473a708b8837
xiaoyu-li-china <1273894172@qq.com>
2026-04-29 19:35:27 +0800

    Fix init_db_script.py import issue

--- init_db_script.py
+++ init_db_script.py
@@ -2,8 +2,9 @@ import os
 os.environ.setdefault('FLASK_ENV', 'production')
 os.environ.setdefault('DATABASE_URL', 'sqlite:///site.db')
 
-from run import app
-from app import db
+from app import create_app, db
+
+app = create_app()
 
 with app.app_context():
     db.create_all()
